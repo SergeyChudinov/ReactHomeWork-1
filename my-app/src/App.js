a2e88e07d61b4a618456e9cac04a222d1ff69c0b
SergeyChudinov <sergeychudinov1986@gmail.com>
2022-08-05 21:45:27 +0500

    Make lesson-1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Message from './Message'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Message text={'Просто текст!!!'}/>
+    </div>
+  );
+}
+
+export default App;
